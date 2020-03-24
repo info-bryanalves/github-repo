1e83e62d499eb3afae1af8f4f3277f5e141dcc81
Bryan Alves <info.bryanalves@gmail.com>
2020-03-24 13:19:32 -0300

    Buscar dados do usuário na API do GitHub

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import logo from '../assets/logo.png'
 import search from '../assets/search.png'
@@ -7,18 +7,45 @@ import './header.css'
 
 export default function Header() {
 
+  const [username, setUsername] = useState('')
+  const inputRef = useRef(null)
+
+  const handleGetUser = async (e) => {
+    e.preventDefault()
+
+    if (!username) {
+      inputRef.current.focus()
+      return false
+    }
+
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    const data = await response.json()
+
+    if (data) {
+      alert('Usuário não encontrado!')
+    }
+    
+    console.log(data)
+  }
+
   const buttonStyle = {
     backgroundImage: `url(${search})`
   }
 
   return (
     <header id="page-header">
-      <form>
+      <form onSubmit={handleGetUser}>
         <img src={logo} alt="" />
-        <input type="text" />
+        <input
+          ref={inputRef}
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          placeholder="Digite seu usuário do GitHub"
+        />
         <button
-        type="submit"
-        style={buttonStyle}></button>
+          type="submit"
+          style={buttonStyle}></button>
       </form>
     </header>
   )
